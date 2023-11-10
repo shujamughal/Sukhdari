8ef84b21b1319ab7400d51c29df7490ae895e10b
HAFIZ MUHAMMAD ARSLAN <100121931+arsla992@users.noreply.github.com>
2023-11-10 20:15:57 +0500

    New Files are added to show

---
+++ DataAccess/Data/CategoryAttributes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class CategoryAttributes
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Key { get; set; } //It's the attribute name
+
+  
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        private List<CategoryAttributes> Attributes { get; set; } = new List<CategoryAttributes>();
+        public Category Category { get; set; }
+
+    }
+}
---
+++ DataAccess/Data/ProductAttributes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class ProductAttributes
+    {
+        [Key]
+        public int AttributeId { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public bool Fixed { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
