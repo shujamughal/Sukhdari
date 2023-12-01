f5fe824fe61b9910225a33c2525d6a6e93b87bfc
bcsf20m016 <bcsf20m016@pucit.edu.pk>
2023-12-01 15:17:56 +0500

    Filter products based on specific category in server side and improved Bulk products component

--- Sukhdari_Server/wwwroot/js/table.js
+++ Sukhdari_Server/wwwroot/js/table.js
@@ -1,6 +1,14 @@
-﻿function ProductTable() {
+﻿function initializeDataTables() {
     $("#product_table").DataTable();
-}
-function CategoryTable() {
     $("#category_table").DataTable();
 }
+
+window.onafterload = function () {
+    initializeDataTables();
+};
+
+window.onunload = function () {
+    // Optionally, destroy DataTables when leaving the page to prevent memory leaks
+    $("#product_table").DataTable().destroy();
+    $("#category_table").DataTable().destroy();
+};
