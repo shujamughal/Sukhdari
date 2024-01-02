46985211e7f9040249b824c7a90c69aeb2fc9a9e
HAFIZ MUHAMMAD ARSLAN <100121931+arsla992@users.noreply.github.com>
2024-01-02 16:34:49 +0500

    Add files via upload

---
+++ Sukhdari/Sukhdari/Service/IService/IDbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sukhdari.Service.IService
+{
+    public interface IDbInitializer
+    {
+        void Initalize();
+    }
+}
